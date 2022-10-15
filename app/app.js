ab9a9787fa49b63a35ca9d330babed59574dca58
Alex Tang <alextoronto66@gmail.com>
2022-10-15 11:46:37 -0400

    Passport Setup + login & registration ejs pages

--- app/app.js
+++ app/app.js
@@ -9,6 +9,17 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Auth Step 1 - import modules
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import flash from 'connect-flash';
+
+//Auth Step 2 - define auth strategy
+let localStrategy = passportLocal.Strategy;
+
+//Auth Step 3 - import the user model
+import User from './models/user.js';
+
 // Import Mongoose Module
 import mongoose from 'mongoose';
 
@@ -42,12 +53,32 @@ app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname,'/client')));
 app.use(express.static(path.join(__dirname,'../public')));
+
+
+//Auth Step 4 - Setup Express Session
+
 app.use(session({
     secret: Secret,
     saveUninitialized: false, 
     resave: false
 }));
 
+//Auth Step5 - Setup Flash
+app.use(flash());
+
+//Auth Step6 - Initialize Passprot and Session
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Auth Step7 - Implement the Auth Strategy
+passport.use(User.createStrategy());
+
+
+//Auth Step8 - Setup serialization and deserialization
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.serializeUser());
+
+ 
 // Use Routes
 app.use('/', indexRouter);
 app.use('/', movieRouter);
