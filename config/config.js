0b38352b714fd805bcba2da19e7898e9c6d2ed44
Alex Tang <alextoronto66@gmail.com>
2022-10-15 11:47:25 -0400

    passport + login + registration

--- config/config.js
+++ config/config.js
@@ -1,2 +1,3 @@
 export const Secret = "MySecret";
-export const MongoURI  = "mongodb://127.0.0.1/media"
+//export const MongoURI  = "mongodb://127.0.0.1/media"
+export const MongoURI = "mongodb+srv://WebDevAlex:7wYdpy5IY2bHySlN@cluster0.zkqspsk.mongodb.net/media?retryWrites=true&w=majority";
