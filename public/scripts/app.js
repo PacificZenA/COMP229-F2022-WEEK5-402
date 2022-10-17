0afcaf0fa5cd939ef9ea76525fe5e4074ccfe844
Alex Tang <alextoronto66@gmail.com>
2022-10-17 12:24:01 -0400

    Added picture to home, and pull javascript/css document from previous assignment

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,11 +1,18 @@
-//IIFE
-(function()
-{
+/* File name: app.js
+Student Name: Alex Tang
+StudentID: 301238850
+Date: Oct/17/2022
+*/
 
-    function Start()
-    {
-        console.log("App Started!");
+(function () {
+    function Start() {
+      console.log("App Started");
     }
-
-    window.addEventListener('load', Start);
-})();
+    window.addEventListener("load", Start);
+  })();
+  
+  //Function for redirecting to home page
+  function redirectToHome() {
+    window.location.href = "http://localhost:3000/home";
+  }
+  
